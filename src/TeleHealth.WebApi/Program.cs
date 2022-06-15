43568a012d2f1b8e5cc4e97e216abad7be279d24
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-06-15 11:27:39 -0500

    Adding Jasper

--- src/TeleHealth.WebApi/Program.cs
+++ src/TeleHealth.WebApi/Program.cs
@@ -1,5 +1,11 @@
+using Baseline.Dates;
+using Jasper;
+using Jasper.ErrorHandling;
+using Jasper.Persistence.Marten;
 using Marten;
 using Marten.Events.Projections;
+using Marten.Exceptions;
+using Npgsql;
 using Oakton;
 using TeleHealth.Common;
 
@@ -8,8 +14,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.ApplyOaktonExtensions();
 
 // Add services to the container.
+builder.Host.UseJasper(opts =>
+{
+    // I'm choosing to process any ChartingFinished event messages
+    // in a separate, local queue with persistent messages for the inbox/outbox
+    opts.PublishMessage<ChartingFinished>()
+        .ToLocalQueue("charting")
+        .DurablyPersistedLocally();
+    
+    // If we encounter a concurrency exception, just try it immediately 
+    // up to 3 times total
+    opts.Handlers.OnException<ConcurrencyException>().RetryNow(3); 
+    
+    // It's an imperfect world, and sometimes transient connectivity errors
+    // to the database happen
+    opts.Handlers.OnException<NpgsqlException>()
+        .RetryWithCooldown(50.Milliseconds(), 100.Milliseconds(), 250.Milliseconds());
+});
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,12 +42,16 @@ builder.Services.AddMarten(opts =>
 {
     // This would typically come from configuration
     opts.Connection(ConnectionSource.ConnectionString);
-    
-    // TODO -- register projections
-    
+
     opts.Projections.Add<AppointmentProjection>(ProjectionLifecycle.Inline);
     opts.Projections.SelfAggregate<ProviderShift>(ProjectionLifecycle.Inline);
-});
+})
+    // I added this to enroll Marten in the Jasper outbox
+    .IntegrateWithJasper()
+    
+    // I also added this to opt into events being forward to
+    // the Jasper outbox during SaveChangesAsync()
+    .EventForwardingToJasper();
 
 var app = builder.Build();
 
@@ -35,8 +63,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
-app.MapControllers();
+app.MapControllers(); // I'm sticking with controllers for now
 
 // This is using Oakton for command running
 await app.RunOaktonCommands(args);
